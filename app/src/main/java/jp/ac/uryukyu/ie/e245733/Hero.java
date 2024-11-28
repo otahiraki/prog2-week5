dd06411500db81e3f61b71aeabb6d19561b13955
otahira <otahiraki@ootakainonotobukkukonpyuta.local>
2024-11-28 14:56:36 +0900

    ggg

--- app/src/main/java/jp/ac/uryukyu/ie/e245733/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245733/Hero.java
@@ -1,16 +1,16 @@
 package jp.ac.uryukyu.ie.e245733;
+
 public class Hero extends LivingThing {
-    // コンストラクタ
     public Hero(String name, int hitPoint, int attack) {
-        super(name, hitPoint, attack); // 親クラスのコンストラクタを呼び出す
+        super(name, hitPoint, attack); // 親クラスのコンストラクタを呼び出し
     }
 
-    // woundedメソッドを上書き
     @Override
     public void wounded(int damage) {
-        super.wounded(damage); // 親クラスのダメージ処理
+        super.wounded(damage); // 親クラスのwoundedメソッドを呼び出し
         if (isDead()) {
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
 }
+
